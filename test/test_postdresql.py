3ac589cfb27091e195701e994f00543e3b0c0d74
Kyle Hickey <kylehickey777@gmail.com>
2025-07-22 14:52:36 -0400

    added test functions

---
+++ test/test_postdresql.py
@@ -0,0 +1,7 @@
+'''def test_select_all():
+    """
+    Simple test example for an SQLite3 Query
+    """
+    from SQL import query
+    res = query("select * from test_table;").fetchall()
+    assert len(res) > 0'''
--- test/test_sqlite3.py
+++ test/test_sqlite3.py
@@ -1,3 +1,26 @@
+#update, delete
+
+def get_size():
+    from SQL import query
+    size = query("select * from test_table;").fetchall()
+    return len(size)
+
+def insert_into():
+    from SQL import query
+    from databasebuild import random_string
+    query("insert into test_table (first_name, last_name, address, city) values (?, ?, ?, ?);",
+          (random_string(10), random_string(10), random_string(20), random_string(15)))
+
+def get_one():
+    from SQL import query
+    res = query("select * from test_table limit 1;").fetchall()
+    return len(res)   
+
+def get_some():
+    from SQL import query
+    res = query("select * from test_table limit 10;").fetchall()
+    return len(res)
+
 def test_import_module():
     """
     Simple test example for an SQLite3 Query
@@ -12,3 +35,26 @@ def test_import_module():
     res = query("select 1 as A;").fetchall()
     a = res.pop()
     assert a.A == 1
+
+def test_select_all():
+    from SQL import query
+    res = get_size()
+    assert res > 0
+
+def test_insert_into():
+    from SQL import query
+    count = get_size()
+    insert_into()
+    count2 = get_size()
+    assert count2 == count + 1
+    
+def test_select_one():
+    from SQL import query
+    res = get_one()
+    assert res == 1
+
+def test_select_some():
+    from SQL import query
+    res = get_some()
+    assert res == 10
+
